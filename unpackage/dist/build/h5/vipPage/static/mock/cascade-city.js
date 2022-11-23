59e93999323f1b7205113c7054935157b45339bf
ahua666 <2524329629@qq.com>
2022-11-23 23:10:13 +0800

    vercel部署vip版本

---
+++ unpackage/dist/build/h5/vipPage/static/mock/cascade-city.js
@@ -0,0 +1,68 @@
+export default [
+  {
+    text: '广东省',
+    value: '440000',
+    children: [
+      {
+        text:'广州市',
+        value:'440100',
+        children: [
+          {text:'荔湾区',value:'440103'},
+          {text:'越秀区',value:'440104'},
+          {text:'海珠区',value:'440105'},
+          {text:'天河区',value:'440106'},
+          {text:'白云区',value:'440111'},
+          {text:'黄埔区',value:'440112'},
+          {text:'番禺区',value:'440113'},
+          {text:'花都区',value:'440114'},
+          {text:'南沙区',value:'440115'},
+          {text:'从化区',value:'440117'},
+          {text:'增城区',value:'440118'}
+        ]
+      },
+      {text:'韶关市',value:'440200'},
+      {
+        text:'深圳市',
+        value:'440300',
+        children: [
+          {text:'罗湖区',value:'440303'},
+          {text:'福田区',value:'440304'},
+          {text:'南山区',value:'440305'},
+          {text:'宝安区',value:'440306'},
+          {text:'龙岗区',value:'440307'},
+          {text:'盐田区',value:'440308'},
+          {text:'龙华区',value:'440309'},
+          {text:'坪山区',value:'440310'}
+        ]
+      },
+      {text:'珠海市',value:'440400'},
+      {text:'汕头市',value:'440500'},
+      {
+        text:'佛山市',
+        value:'440600',
+        children: [
+          {text:'禅城区',value:'440604'},
+          {text:'南海区',value:'440605'},
+          {text:'顺德区',value:'440606'},
+          {text:'三水区',value:'440607'},
+          {text:'高明区',value:'440608'}
+        ]
+      },
+      {text:'江门市',value:'440700'},
+      {text:'湛江市',value:'440800'},
+      {text:'茂名市',value:'440900'},
+      {text:'肇庆市',value:'441200'},
+      {text:'惠州市',value:'441300'},
+      {text:'梅州市',value:'441400'},
+      {text:'汕尾市',value:'441500'},
+      {text:'河源市',value:'441600'},
+      {text:'阳江市',value:'441700'},
+      {text:'清远市',value:'441800'},
+      {text:'东莞市',value:'441900'},
+      {text:'中山市',value:'442000'},
+      {text:'潮州市',value:'445100'},
+      {text:'揭阳市',value:'445200'},
+      {text:'云浮市',value:'445300'},
+    ]
+  }
+]
