a695243cf470724a4f4da8f5e4dc3ac790a64b5c
qixv-0506 <935852521@qq.com>
2023-08-08 21:41:37 +0800

    修复分段器动态赋值不生效的bug
    (cherry picked from commit 652e77f7e353bddde500bdaa708541f4ad700be7)

--- tuniao-ui/components/tn-subsection/tn-subsection.vue
+++ tuniao-ui/components/tn-subsection/tn-subsection.vue
@@ -126,217 +126,220 @@
 						}
 					})
 				},
-				current: {
-					handler(val) {
-						this.currentIndex = val
-						this.changeSectionStatus(val)
-					},
-					immediate: true
+				immediate: true,
+				deep: true
+			},
+			current: {
+				handler(val) {
+					this.currentIndex = val
+					this.changeSectionStatus(val)
+				},
+				immediate: true
+			}
+		},
+		created() {
+			// 将list的数据，传入listInfo数组
+			// 接受直接数组形式，或者数组元素为对象的形式，如：['开启', '关闭'],或者[{name: '开启'}, {name: '关闭'}]
+			this.listInfo = this.list.map((item, index) => {
+				if (typeof item !== 'object') {
+					let obj = {
+						width: 0,
+						name: item
+					}
+					return obj
+				} else {
+					item.width = 0
+					return obj
+				}
+			})
+		},
+		computed: {
+			// 设置mode=subsection时，滑块没有样式
+			noBorderRight() {
+				return index => {
+					if (this.mode !== 'subsection') return
+					let clazz = ''
+					// 不显示右边的边距
+					if (index < this.list.length - 1) clazz += ' tn-subsection__item--none-border-right'
+					// 显示整个组件的左右边圆角
+					if (index === 0) clazz += ' tn-subsection__item--first'
+					if (index === this.list.length - 1) clazz += ' tn-subsection__item--last'
+					return clazz
 				}
 			},
-			created() {
-				// 将list的数据，传入listInfo数组
-				// 接受直接数组形式，或者数组元素为对象的形式，如：['开启', '关闭'],或者[{name: '开启'}, {name: '关闭'}]
-				this.listInfo = this.list.map((item, index) => {
-					if (typeof item !== 'object') {
-						let obj = {
-							width: 0,
-							name: item
-						}
-						return obj
-					} else {
-						item.width = 0
-						return obj
-					}
-				})
-			},
-			computed: {
-				// 设置mode=subsection时，滑块没有样式
-				noBorderRight() {
-					return index => {
-						if (this.mode !== 'subsection') return
-						let clazz = ''
-						// 不显示右边的边距
-						if (index < this.list.length - 1) clazz += ' tn-subsection__item--none-border-right'
-						// 显示整个组件的左右边圆角
-						if (index === 0) clazz += ' tn-subsection__item--first'
-						if (index === this.list.length - 1) clazz += ' tn-subsection__item--last'
-						return clazz
-					}
-				},
-				// 文字的样式
-				textStyle() {
-					return index => {
-						let style = {}
-						// 设置字体颜色
-						if (index === this.currentIndex) {
-							style.color = this.activeColor
-						} else {
-							style.color = this.inactiveColor
-						}
-						// 字体加粗
-						if (index === this.currentIndex && this.bold) style.fontWeight = 'bold'
-						// 文字大小
-						style.fontSize = (this.fontSize || 26) + this.fontUnit
-						return style
-					}
-				},
-				// 每个分段器item的样式
-				itemStyle() {
-					return index => {
-						let style = {}
-						if (this.fontSizeStyle) {
-							style.fontSize = this.fontSizeStyle
-						}
-						if (this.mode === 'subsection') {
-							// 设置border的样式
-							style.borderColor = this.buttonColor
-							style.borderWidth = '1rpx'
-							style.borderStyle = 'solid'
-						}
-						return style
-					}
-				},
-				// mode = button时，设置外层view的样式
-				subsectionStyle() {
+			// 文字的样式
+			textStyle() {
+				return index => {
 					let style = {}
-					style.height = this.height + 'rpx'
-					if (this.mode === 'button') {
-						style.backgroundColor = this.backgroundColorStyle
-						style.padding = `${this.buttonPadding}px`
-						style.borderRadius = `${this.borderRadius}rpx`
+					// 设置字体颜色
+					if (index === this.currentIndex) {
+						style.color = this.activeColor
+					} else {
+						style.color = this.inactiveColor
+					}
+					// 字体加粗
+					if (index === this.currentIndex && this.bold) style.fontWeight = 'bold'
+					// 文字大小
+					style.fontSize = (this.fontSize || 26) + this.fontUnit
+					return style
+				}
+			},
+			// 每个分段器item的样式
+			itemStyle() {
+				return index => {
+					let style = {}
+					if (this.fontSizeStyle) {
+						style.fontSize = this.fontSizeStyle
+					}
+					if (this.mode === 'subsection') {
+						// 设置border的样式
+						style.borderColor = this.buttonColor
+						style.borderWidth = '1rpx'
+						style.borderStyle = 'solid'
 					}
 					return style
-				},
-				// mode = button时，设置外层view的背景class
-				subsectionBackgroundColorClass() {
-					let clazz = ''
-					if (this.mode === 'button' && this.backgroundColorClass) {
-						clazz = this.backgroundColorClass
-					}
-					return clazz
-				},
-				itemBarClass() {
-					let clazz = ''
-					const buttonBgClass = this.$tn.color.getBackgroundColorInternalClass(this.buttonColor)
-					if (this.animation) {
-						clazz += ' tn-subsection__bg__animation'
-						if (this.animationType) {
-							clazz += ` tn-subsection__bg__animation--${this.animationType}`
-						}
-					}
-					if (buttonBgClass) {
-						clazz += ` ${buttonBgClass}`
-					}
-					return clazz
-				},
-				// 滑块样式
-				itemBarStyle() {
-					let style = {}
-					const buttonBgStyle = this.$tn.color.getBackgroundColorStyle(this.buttonColor)
-					if (buttonBgStyle) {
-						style.backgroundColor = this.buttonColor
-					}
-					style.zIndex = 1
-					if (this.mode === 'button') {
-						style.borderRadius = `${this.borderRadius}rpx`
-						style.bottom = `${this.buttonPadding}px`
-						style.height = (this.height - (this.buttonPadding * 4)) + 'rpx'
-						style.zIndex = 0
-					}
-					return Object.assign(this.itemBgStyle, style)
 				}
 			},
-			mounted() {
-				// 等待加载组件完成
+			// mode = button时，设置外层view的样式
+			subsectionStyle() {
+				let style = {}
+				style.height = this.height + 'rpx'
+				if (this.mode === 'button') {
+					style.backgroundColor = this.backgroundColorStyle
+					style.padding = `${this.buttonPadding}px`
+					style.borderRadius = `${this.borderRadius}rpx`
+				}
+				return style
+			},
+			// mode = button时，设置外层view的背景class
+			subsectionBackgroundColorClass() {
+				let clazz = ''
+				if (this.mode === 'button' && this.backgroundColorClass) {
+					clazz = this.backgroundColorClass
+				}
+				return clazz
+			},
+			itemBarClass() {
+				let clazz = ''
+				const buttonBgClass = this.$tn.color.getBackgroundColorInternalClass(this.buttonColor)
+				if (this.animation) {
+					clazz += ' tn-subsection__bg__animation'
+					if (this.animationType) {
+						clazz += ` tn-subsection__bg__animation--${this.animationType}`
+					}
+				}
+				if (buttonBgClass) {
+					clazz += ` ${buttonBgClass}`
+				}
+				return clazz
+			},
+			// 滑块样式
+			itemBarStyle() {
+				let style = {}
+				const buttonBgStyle = this.$tn.color.getBackgroundColorStyle(this.buttonColor)
+				if (buttonBgStyle) {
+					style.backgroundColor = this.buttonColor
+				}
+				style.zIndex = 1
+				if (this.mode === 'button') {
+					style.borderRadius = `${this.borderRadius}rpx`
+					style.bottom = `${this.buttonPadding}px`
+					style.height = (this.height - (this.buttonPadding * 4)) + 'rpx'
+					style.zIndex = 0
+				}
+				return Object.assign(this.itemBgStyle, style)
+			}
+		},
+		mounted() {
+			// 等待加载组件完成
+			setTimeout(() => {
+				this.getTabsInfo()
+			}, 10)
+		},
+		methods: {
+			// 改变滑块样式
+			changeSectionStatus(val) {
+				if (this.mode === 'subsection') {
+					// 根据滑块在最左和最右时，显示对应的圆角
+					if (val === this.list.length - 1) {
+						this.itemBgStyle.borderRadius = `0 ${this.buttonPadding}px ${this.buttonPadding}px 0`
+					}
+					if (val === 0) {
+						this.itemBgStyle.borderRadius = `${this.buttonPadding}px 0 0 ${this.buttonPadding}px`
+					}
+					if (val > 0 && val < this.list.length - 1) {
+						this.itemBgStyle.borderRadius = '0'
+					}
+				}
+				// 更新滑块的位置
 				setTimeout(() => {
-					this.getTabsInfo()
+					this.itemBgLeft()
 				}, 10)
-			},
-			methods: {
-				// 改变滑块样式
-				changeSectionStatus(val) {
-					if (this.mode === 'subsection') {
-						// 根据滑块在最左和最右时，显示对应的圆角
-						if (val === this.list.length - 1) {
-							this.itemBgStyle.borderRadius = `0 ${this.buttonPadding}px ${this.buttonPadding}px 0`
-						}
-						if (val === 0) {
-							this.itemBgStyle.borderRadius = `${this.buttonPadding}px 0 0 ${this.buttonPadding}px`
-						}
-						if (val > 0 && val < this.list.length - 1) {
-							this.itemBgStyle.borderRadius = '0'
-						}
-					}
-					// 更新滑块的位置
-					setTimeout(() => {
-						this.itemBgLeft()
-					}, 10)
-					if (this.vibrateShort && !this.firstVibrateShort) {
-						// 使手机产生短促震动，微信小程序有效，APP(HX 2.6.8)和H5无效
-						// #ifndef H5
-						uni.vibrateShort();
-						// #endif
-					}
-					this.firstVibrateShort = false
-				},
-				// 获取各个tab的节点信息
-				getTabsInfo() {
-					let view = uni.createSelectorQuery().in(this)
-					for (let i = 0; i < this.list.length; i++) {
-						view.select('.section-item-' + i).boundingClientRect()
-					}
-					view.exec(res => {
-						// 如果没有获取到，则重新获取
-						if (!res.length) {
-							setTimeout(() => {
-								this.getTabsInfo()
-								return
-							}, 10)
-						}
-						// 将每个分段器的宽度放入listInfo中
-						res.map((item, index) => {
-							this.listInfo[index].width = item.width
-						})
-						// 初始化滑块的宽度
-						if (this.mode === 'subsection') {
-							this.itemBgStyle.width = this.listInfo[0].width + 'px'
-						} else if (this.mode === 'button') {
-							this.itemBgStyle.width = this.listInfo[0].width + 'px'
-						}
-
-						// 初始化滑块的位置
-						this.itemBgLeft()
-					})
-				},
-				// 设置滑块的位置
-				itemBgLeft() {
-					let left = 0
-					// 计算当前活跃item到组件左边的距离
-					this.listInfo.map((item, index) => {
-						if (index < this.currentIndex) left += item.width
-					})
-					// 根据不同的模式，计算滑块的位置
-					if (this.mode === 'subsection') {
-						this.itemBgStyle.left = left + 'px'
-					} else if (this.mode === 'button') {
-						this.itemBgStyle.left = left + this.buttonPadding + 'px'
-					}
-				},
-
-				// 点击事件
-				click(index) {
-					// 不允许点击当前激活的选项
-					if (index === this.currentIndex) return
-					this.currentIndex = index
-					this.changeSectionStatus(index)
-					this.$emit('change', {
-						index: Number(index),
-						name: this.listInfo[index]['name']
-					})
+				if (this.vibrateShort && !this.firstVibrateShort) {
+					// 使手机产生短促震动，微信小程序有效，APP(HX 2.6.8)和H5无效
+					// #ifndef H5
+					uni.vibrateShort();
+					// #endif
 				}
+				this.firstVibrateShort = false
+			},
+			// 获取各个tab的节点信息
+			getTabsInfo() {
+				let view = uni.createSelectorQuery().in(this)
+				for (let i = 0; i < this.list.length; i++) {
+					view.select('.section-item-' + i).boundingClientRect()
+				}
+				view.exec(res => {
+					// 如果没有获取到，则重新获取
+					if (!res.length) {
+						setTimeout(() => {
+							this.getTabsInfo()
+							return
+						}, 10)
+					}
+					// 将每个分段器的宽度放入listInfo中
+					res.map((item, index) => {
+						this.listInfo[index].width = item.width
+					})
+					// 初始化滑块的宽度
+					if (this.mode === 'subsection') {
+						this.itemBgStyle.width = this.listInfo[0].width + 'px'
+					} else if (this.mode === 'button') {
+						this.itemBgStyle.width = this.listInfo[0].width + 'px'
+					}
+
+					// 初始化滑块的位置
+					this.itemBgLeft()
+				})
+			},
+			// 设置滑块的位置
+			itemBgLeft() {
+				let left = 0
+				// 计算当前活跃item到组件左边的距离
+				this.listInfo.map((item, index) => {
+					if (index < this.currentIndex) left += item.width
+				})
+				// 根据不同的模式，计算滑块的位置
+				if (this.mode === 'subsection') {
+					this.itemBgStyle.left = left + 'px'
+				} else if (this.mode === 'button') {
+					this.itemBgStyle.left = left + this.buttonPadding + 'px'
+				}
+			},
+
+			// 点击事件
+			click(index) {
+				// 不允许点击当前激活的选项
+				if (index === this.currentIndex) return
+				this.currentIndex = index
+				this.changeSectionStatus(index)
+				this.$emit('change', {
+					index: Number(index),
+					name: this.listInfo[index]['name']
+				})
 			}
 		}
+	}
 </script>
 
 <style lang="scss" scoped>
